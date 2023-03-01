50af099d46a544da246ca66a01eccf7d8888bf78
Ra Wai Aung <rawaiucsm@gmail.com>
2023-03-01 23:18:57 +0630

    events

--- src/index.js
+++ src/index.js
@@ -17,11 +17,9 @@ app.use(express.static(publicDirectoryPath));
 let count = 0;
 io.on("connection", (socket) => {
   console.log("New Websocket connection");
-  socket.emit("countUpdated", count);
-  socket.on("increment", () => {
-    count++;
-    // socket.emit("countUpdated", count);
-    io.emit("countUpdated", count);
+  socket.emit("message", "Welcome !");
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
   });
 });
 server.listen(port, () => {
