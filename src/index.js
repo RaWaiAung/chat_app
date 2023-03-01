c5ca43830b7a06621bfbd9e652312a15d9d68d2f
Ra Wai Aung <rawaiucsm@gmail.com>
2023-03-01 23:54:51 +0630

    broadcast

--- src/index.js
+++ src/index.js
@@ -17,9 +17,16 @@ app.use(express.static(publicDirectoryPath));
 let count = 0;
 io.on("connection", (socket) => {
   console.log("New Websocket connection");
+
   socket.emit("message", "Welcome !");
+  socket.broadcast.emit("message", "A new user joined !");
+
   socket.on("sendMessage", (message) => {
     io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
   });
 });
 server.listen(port, () => {
